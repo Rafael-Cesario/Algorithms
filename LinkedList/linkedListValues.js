cae2d8a7ce0553dddacc1249f9394f0b8f050253
Rafael <rafaeloliveiracds@hotmail.com>
2022-10-11 07:21:47 -0300

    Linked List Values

--- LinkedList/linkedListValues.js
+++ LinkedList/linkedListValues.js
@@ -1,8 +1,42 @@
+class Node {
+	constructor(value) {
+		this.value = value;
+		this.next = null;
+	}
+}
+
 // Write a function, linkedListValues, that takes in the head of a linked list as an argument. The function should return an array containing all values of the nodes in the linked list.
 
+// const linkedListValues = (root) => {
+// 	const values = [];
+
+// 	let current = root;
+
+// 	while (current) {
+// 		values.push(current.value);
+// 		current = current.next;
+// 	}
+
+// 	return values;
+// };
+
+// time = O(n)
+// space = o(n)
+
 const linkedListValues = (root) => {
-  
-}
+	const values = [];
+
+	fillValues(root, values);
+
+	return values;
+};
+
+const fillValues = (root, values) => {
+	if (!root) return;
+
+	values.push(root.value);
+	fillValues(root.next, values);
+};
 
 const a = new Node('a');
 const b = new Node('b');
