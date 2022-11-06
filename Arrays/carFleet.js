905a49161c3c8fc81f0b856676c9184395d8e66e
Rafael <rafaeloliveiracds@gmail.com>
2022-11-06 17:45:04 -0300

    Car Fleet

---
+++ Arrays/carFleet.js
@@ -0,0 +1,44 @@
+// There are n cars going to the same destination along a one-lane road. The destination is target miles away.
+
+// You are given two integer array position and speed, both of length n, where position[i] is the position of the ith car and speed[i] is the speed of the ith car (in miles per hour).
+
+// A car can never pass another car ahead of it, but it can catch up to it and drive bumper to bumper at the same speed. The faster car will slow down to match the slower car's speed. The distance between these two cars is ignored (i.e., they are assumed to have the same position).
+
+// A car fleet is some non-empty set of cars driving at the same position and same speed. Note that a single car is also a car fleet.
+
+// If a car catches up to a car fleet right at the destination point, it will still be considered as one car fleet.
+
+// Return the number of car fleets that will arrive at the destination.
+
+const carFleet = (target, position, speed) => {
+	const stack = [];
+	const positionSpeed = position.map((car, i) => [car, speed[i]]);
+
+	// sort array O(n log n);
+	for (let car in positionSpeed) {
+		let max = car;
+		let i = Number(car) + 1;
+
+		while (i < positionSpeed.length) {
+			if (positionSpeed[max][0] < positionSpeed[i][0]) max = i;
+			i++;
+		}
+
+		const temp = positionSpeed[car];
+		positionSpeed[car] = positionSpeed[max];
+		positionSpeed[max] = temp;
+	}
+
+	for (let i = positionSpeed.length - 1; i >= 0; i--) {
+		const [pos, speed] = positionSpeed[i];
+		const arriveAt = (target - pos) / speed;
+
+		stack.push(arriveAt);
+
+		if (stack.length >= 2 && stack[stack.length - 1] >= stack[stack.length - 2]) stack.pop();
+	}
+
+	return stack.length;
+};
+
+console.log(carFleet(12, [10, 8, 0, 5, 3], [2, 4, 1, 1, 3]));
